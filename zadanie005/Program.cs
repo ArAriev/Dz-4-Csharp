40d08cca67ee159e2ead9bc6a28852381cfca2ff
Aleksandr Onufriev <pskmp@yandex.ru>
2022-10-15 22:31:02 +0300

    dobavil zadanie005

---
+++ zadanie005/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задание 5. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] array = new int [10];
+int summa = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i] + ", ");
+    if (i % 2 != 0)
+    {
+        summa = summa + array[i];
+    }
+}
+Console.WriteLine($" cуммa чисел одномерного массива стоящих на нечетной позиции = {summa}");
+
